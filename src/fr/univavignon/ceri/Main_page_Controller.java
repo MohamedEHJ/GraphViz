c252fcc12219ebfe2596afe9b5fa9032e4763d9b
EL HAJOUI Mohamed <46054862+MohamedEHJ@users.noreply.github.com>
2021-02-15 14:45:24 +0100

    V1: Adding scene Change

--- src/fr/univavignon/ceri/Main_page_Controller.java
+++ src/fr/univavignon/ceri/Main_page_Controller.java
@@ -4,9 +4,13 @@ import com.sun.glass.ui.CommonDialogs;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
 import java.io.File;
 import java.io.IOException;
@@ -20,9 +24,37 @@ public class Main_page_Controller {
     @FXML
     public Button search_btn;
 
+    /**
+     * Manage the scene change when clicking on "visualisation" button.
+     * Send the file in argument to the second scene too.
+     * @param actionEvent
+     * @throws IOException
+     */
     public void visualisation(ActionEvent actionEvent) throws IOException {
-        AnchorPane a = FXMLLoader.load(getClass().getResource("second_page.fxml"));
-    }
+        System.out.println("You clicked on 'Visualisation' button!");
+
+
+        // A ENLEVER A ENLEVER A ENLEVER A ENLEVER A ENLEVER A ENLEVER A ENLEVER
+//        fileChoosen = new File("C:\\Users\\AZ\\Documents\\graphs\\crawl40.graphml");
+        // A ENLEVER A ENLEVER A ENLEVER A ENLEVER A ENLEVER A ENLEVER A ENLEVER A ENLEVER
+
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("second_page.fxml"));
+        Parent home_page_parent = loader.load();
+        Scene home_page_scene = new Scene(home_page_parent);
+        Stage app_stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        if (true) {
+            // here we should verify the extension of the file. The button would be lock if the format is not good,
+            // else we will be able to change scene
+            /**
+             * pass the information to the home page controller.
+             */
+            SecondPage hpController = loader.getController();
+//            hpController.receiveFile(fileChoosen);
+
+            app_stage.setScene(home_page_scene);
+            app_stage.show();
+        }    }
 
     /**
      * Manage the filechooser button.
@@ -46,6 +78,8 @@ public class Main_page_Controller {
             System.out.println("file is not valid");
         }
     }
+
+
 
 
 }
